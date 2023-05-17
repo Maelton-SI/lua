37075d9899e5e892330a4fa81618c52d7de1ae46
Emesson Horácio dos Santos <110675392+emessonh@users.noreply.github.com>
2023-05-17 15:17:14 -0300

    criacao_de_modulo

---
+++ outros_exemplos/modulo/main.lua
@@ -0,0 +1,3 @@
+local MeuModulo = require("meumodulo") -- Função require importa o modulo 
+
+MeuModulo.funcaoPublica()              -- Chamando a função pública do módulo           
---
+++ outros_exemplos/modulo/meumodulo.lua
@@ -0,0 +1,10 @@
+-- Definindo a tabela do módulo
+local MeuModulo = {}
+
+-- Função pública
+function MeuModulo.funcaoPublica()
+    print("Esta eh a funcao publica do modulo")
+end
+
+-- Retornando a tabela do módulo
+return MeuModulo
